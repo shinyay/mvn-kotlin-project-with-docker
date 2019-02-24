37fed9d7d819c8ed6b14366771bdf72934d27054
shinyay <shinya.com@gmail.com>
2019-02-24 22:41:32 +0900

    Modify script using env value

--- generate-kotlin-project.fish
+++ generate-kotlin-project.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/env fish
 
-docker run -it --rm --name my-maven-project -v (pwd):/usr/src/mymaven -w /usr/src/mymaven maven:3.6.0-jdk-11-slim \
-   mvn archetype:generate -DarchetypeGroupId=org.jetbrains.kotlin -DarchetypeArtifactId=kotlin-archetype-jvm -DarchetypeVersion=1.3.21 \
+set -x CONTAINER_NAME mvn-container
+set -x CONTAINER_VERSION 3.6.0-jdk-11-slim
+set -x ARCHETYPE_VERSION 1.3.21
+set -x GROUP_ID io.pivotal.shinyay
+set -x ARTIFACT_ID kotlin-app
+
+docker run -it --rm --name $CONTAINER_NAME -v (pwd):/usr/src/mymaven -w /usr/src/mymaven maven:$CONTAINER_VERSION \
+   mvn archetype:generate -DarchetypeGroupId=org.jetbrains.kotlin -DarchetypeArtifactId=kotlin-archetype-jvm -DarchetypeVersion=$ARCHETYPE_VERSION \
       -DinteractiveMode=false \
-      -DgroupId=io.pivotal.shinyay \
-      -DartifactId=kotlin-app
+      -DgroupId=$GROUP_ID \
+      -DartifactId=$ARTIFACT_ID
 
