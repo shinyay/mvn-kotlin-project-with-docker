e70c04a1d1b89e1c8451f45fa90bd7ea627c9e99
shinyay <shinya.com@gmail.com>
2019-02-25 22:03:26 +0900

    Add local maven repositpry

--- generate-kotlin-project.fish
+++ generate-kotlin-project.fish
@@ -6,7 +6,8 @@ set -x ARCHETYPE_VERSION 1.3.21
 set -x GROUP_ID io.pivotal.shinyay
 set -x ARTIFACT_ID kotlin-app
 
-docker run -it --rm --name $CONTAINER_NAME -v (pwd):/usr/src/mymaven -w /usr/src/mymaven maven:$CONTAINER_VERSION \
+docker volume create --name maven-repo
+docker run -it --rm --name $CONTAINER_NAME -v (pwd):/usr/src/mymaven -v maven-repo:/root/.m2 -w /usr/src/mymaven maven:$CONTAINER_VERSION \
    mvn archetype:generate -DarchetypeGroupId=org.jetbrains.kotlin -DarchetypeArtifactId=kotlin-archetype-jvm -DarchetypeVersion=$ARCHETYPE_VERSION \
       -DinteractiveMode=false \
       -DgroupId=$GROUP_ID \
